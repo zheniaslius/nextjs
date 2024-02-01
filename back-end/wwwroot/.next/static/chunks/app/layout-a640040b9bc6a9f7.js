77bd0f83441f7825ec7588cd4b243560d57dd88c
Yevhen <sluisarchuk@gmail.com>
2024-02-01 23:26:13 +0200

    first commit

---
+++ back-end/wwwroot/.next/static/chunks/app/layout-a640040b9bc6a9f7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{6757:function(e,n,t){Promise.resolve().then(t.t.bind(t,5250,23)),Promise.resolve().then(t.t.bind(t,3385,23)),Promise.resolve().then(t.t.bind(t,9646,23))},3385:function(){}},function(e){e.O(0,[870,971,69,744],function(){return e(e.s=6757)}),_N_E=e.O()}]);
